a33382dc02262da6b1e2ec68bf667fb57489c0a0
Vitu <vitorcsharp@hotmail.com>
2024-02-13 15:27:30 -0300

    new configurations automatic

--- obj/Debug/net8.0/api_cli.AssemblyInfo.cs
+++ obj/Debug/net8.0/api_cli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_cli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+634780d8be1911e03dc42579883c5a0aa4b582c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ffb7306967c81e87dafc77ab96ba59afa9e59d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_cli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_cli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
