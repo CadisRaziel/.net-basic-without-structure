7c8a62c0a5f0a47b49a65ecd5c959a4e62c10e26
Vitu <vitorcsharp@hotmail.com>
2024-02-15 15:18:54 -0300

    Aprendendo sobre configuraçoes

--- obj/Debug/net8.0/api_cli.AssemblyInfo.cs
+++ obj/Debug/net8.0/api_cli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_cli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+873024fe0c44f6a682a6070691cf70263f207c52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d1d5c4586098fd99e9dd8bfc56f270120d0312")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_cli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_cli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
