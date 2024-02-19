b7111ca9519943704e86618213a0e134e9e0077e
Vitu <vitorcsharp@hotmail.com>
2024-02-19 11:21:31 -0300

    new version

--- obj/Debug/net8.0/api_cli.AssemblyInfo.cs
+++ obj/Debug/net8.0/api_cli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_cli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f60bce7e37dff135fdf44e46dc18cbf2c1eba2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b16cdef4d67a64b64a857f52415414a251e55f7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_cli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_cli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
